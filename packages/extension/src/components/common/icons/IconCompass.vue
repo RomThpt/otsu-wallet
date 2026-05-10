5fb6506cdca62ecb7f61ac1494fbb4f024b533aa
RomThpt <reeflk8@gmail.com>
2026-05-10 13:53:47 +0200

    feat(extension): add minimal nav icons

---
+++ packages/extension/src/components/common/icons/IconCompass.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="18"
+    height="18"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <circle cx="12" cy="12" r="9" />
+    <path d="M15.5 8.5l-2 5-5 2 2-5z" />
+  </svg>
+</template>
